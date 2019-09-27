c984f21a02376207b586e48d953476b2306f4769
tejaswini-05 <tejaswini.pallamreddy57@gmail.com>
2019-09-27 19:08:09 +0530

    modules

---
+++ Mypackages_arth/basics_art.py
@@ -0,0 +1,9 @@
+def swap(a,b):
+    b=a+b
+    a=b-a
+    b=b-a
+    return a,b
+swap(2,3)
+
+def rem(num,dem):
+    return num%dem
---
+++ greetings.py
@@ -0,0 +1,6 @@
+def hello(name):
+    return "hello "+name
+
+
+def birthday(name):
+    return "happy birthday "+name
